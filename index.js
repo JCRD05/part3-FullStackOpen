748cc796f9b1a54e58dc428aa3edcbe796e63d16
Jose Carlos Ruiz Diaz <josecarlosruizdiaz05@gmail.com>
2026-05-18 17:46:28 -0600

    exercise 3.11

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
